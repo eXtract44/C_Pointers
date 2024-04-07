1f83463144c425e077216111fe594c18eaf36c60
Alex <71541578+eXtract44@users.noreply.github.com>
2024-04-07 16:16:16 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,1 +1,26 @@
+void swapInts(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
+void swapIntsNoThird1(int* a, int* b) {
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+void swapIntsNoThird2(int* a, int* b) {
+	*a = *a * *b;
+	*b = *a / *b;
+	*a = *a / *b;
+}
+
+void genericSwap(void* a, void* b, int size)
+{
+	void* tempMemory = malloc(size);
+	memcpy(tempMemory, a, size);
+	memcpy(a, b, size);
+	memcpy(b, tempMemory, size);
+}
