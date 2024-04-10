035b360a4747f2cbd0400d56e7591abb41f347b3
Alex <71541578+eXtract44@users.noreply.github.com>
2024-04-10 21:54:44 +0200

    Update array.c

--- array.c
+++ array.c
@@ -51,9 +51,45 @@ void findMinMaxValueInArray(int* array,int size, int *min, int*max){
 }
 findMinMaxValueInArray(array,size,&min,&max);
 /********************************************************************************************************************************/
-void updateAverage(void *a, void* b){
- *a = (a* + *b) / 2;
-*a=*b;
+void updateAverage(int a, int b,*avr){
+  float avr =(a + b) / 2.0;
+*avr=avr;
 }
+updateAvarage(a,b,&avr);
+/********************************************************************************************************************************/
+void findMinMaxValueInArray(int* array,int size, int *min, int*max){
+ *min = array[0];
+ *max = array[0];
+  for(int i = 1;i<size;i++){
+      if(array[i] < *min){
+          *min=array[i];
+      }
+      if(array[i] > *max){
+          *max=array[i];
+      }
+    }
+}
+findMinMaxValueInArray(array,size,&min,&max);
+/********************************************************************************************************************************/
+int findIfSumNeighborns(int* array,int size){
+  for(int i = 1;i<size;i++){
+      printf("iter:%d \n", i);
+      printf("array[sum]:%d \n", array[i]);
+      printf("array[add1]:%d \n", array[i-1]);
+      printf("array[add2]:%d \n", array[i+1]);
+      printf("*******************\n");
+      
+      if(array[i] == array[i-1] + array[i+1]){
+          return 1;
+      }
+    }
+    return 0;
+}
+#define SIZE 10
+int main() {
+    int arr[SIZE]= {1,3,7,8,5,11,7,8,17,10};
+    // Write C code here
+    printf("findIfSumNeighborns:%d", findIfSumNeighborns(arr,SIZE));
 
-
+    return 0;
+}
