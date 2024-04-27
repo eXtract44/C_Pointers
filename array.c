0bbf3982886f96c528a4dd642d91b9830fc4b543
Alex <71541578+eXtract44@users.noreply.github.com>
2024-04-27 13:31:17 +0200

    go

--- array.c
+++ array.c
@@ -1,140 +1,140 @@
-/********************************************************************************************************************************/
-int sumArray(int* array,int size){ 
+#include <stdio.h>
+
+
+int sumArray(int *array, int size)
+{
   int sum = 0;
-  if(size < 0){return 0;}
-  for(int i = 0;i<size;i++){
+  if (size < 0)
+  {
+    return 0;
+  }
+  for (int i = 0; i < size; i++)
+  {
     sum = sum + array[i];
   }
-return sum;
+  return sum;
 }
-/********************************************************************************************************************************/
-int multiplyArray(int* array,int size){
+int multiplyArray(int *array, int size)
+{
   int multiply = 0;
-  if(size < 0){return 0;}
-  for(int i = 0;i<size;i++){
+  if (size < 0)
+  {
+    return 0;
+  }
+  for (int i = 0; i < size; i++)
+  {
     multiply = multiply * array[i];
   }
-return sum;
+  return sum;
 }
-/********************************************************************************************************************************/
-bool findIfKeyInArray(int* array,int size,int key){
-  if(size < 0){return 0;}
-  for(int i = 0;i<size;i++){
-    if(key == array[i]){
+int findIfKeyInArray(int *array, int size, int key)
+{
+  if (size < 0)
+  {
+    return 0;
+  }
+  for (int i = 0; i < size; i++)
+  {
+    if (key == array[i])
+    {
       return 1;
     }
   }
   return 0;
 }
-/********************************************************************************************************************************/
-int findMaxValueInArray(int* array,int size){
+int findMaxValueInArray(int *array, int size)
+{
   int temp_data = 0;
-  if(size < 0){return 0;}
-  for(int i = 0;i<size;i++){
-      (array[i]) > (array[i+1]) ? (temp_data=temp_data+array[i]) : (temp_data=temp_data+array[i+1])  
-    }
+  if (size < 0)
+  {
+    return 0;
+  }
+  for (int i = 0; i < size; i++)
+  {
+    (array[i]) > (array[i + 1]) ? (temp_data = temp_data + array[i]) : (temp_data = temp_data + array[i + 1]);
   }
   return temp_data;
 }
-/********************************************************************************************************************************/
-void findMinMaxValueInArray(int* array,int size, int *min, int*max){
- *min = array[0];
- *max = array[0];
-  for(int i = 1;i<size;i++){
-      if(array[i] < *min){
-          *min=array[i];
-      }
-      if(array[i] > *max){
-          *max=array[i];
-      }
+void updateAverage(int a, int b, float *avr)
+{
+  float avr1 = (a + b) / 2.0;
+  *avr = avr1;
+}
+void findMinMaxValueInArray(int *array, int size, int *min, int *max)
+{
+  *min = array[0];
+  *max = array[0];
+  for (int i = 1; i < size; i++)
+  {
+    if (array[i] < *min)
+    {
+      *min = array[i];
     }
-}
-findMinMaxValueInArray(array,size,&min,&max);
-/********************************************************************************************************************************/
-void updateAverage(int a, int b,*avr){
-  float avr =(a + b) / 2.0;
-*avr=avr;
-}
-updateAvarage(a,b,&avr);
-/********************************************************************************************************************************/
-void findMinMaxValueInArray(int* array,int size, int *min, int*max){
- *min = array[0];
- *max = array[0];
-  for(int i = 1;i<size;i++){
-      if(array[i] < *min){
-          *min=array[i];
-      }
-      if(array[i] > *max){
-          *max=array[i];
-      }
+    if (array[i] > *max)
+    {
+      *max = array[i];
     }
+  }
 }
-findMinMaxValueInArray(array,size,&min,&max);
-/********************************************************************************************************************************/
-int findIfSumNeighborns(int* array,int size){
-  for(int i = 1;i<size-1;i++){
-      printf("iter:%d \n", i);
-      printf("array[sum]:%d \n", array[i]);
-      printf("array[add1]:%d \n", array[i-1]);
-      printf("array[add2]:%d \n", array[i+1]);
-      printf("*******************\n");
-      
-      if(array[i] == array[i-1] + array[i+1]){
-          return 1;
-      }
-    }
-    return 0;
-}
-#define SIZE 10
-int main() {
-    int arr[SIZE]= {1,3,7,8,5,11,7,8,17,10};
-    // Write C code here
-    printf("findIfSumNeighborns:%d", findIfSumNeighborns(arr,SIZE));
+int findIfSumNeighborns(int *array, int size)
+{
+  for (int i = 1; i < size - 1; i++)
+  {
+    printf("iter:%d \n", i);
+    printf("array[sum]:%d \n", array[i]);
+    printf("array[add1]:%d \n", array[i - 1]);
+    printf("array[add2]:%d \n", array[i + 1]);
+    printf("*******************\n");
 
-    return 0;
-}
-/********************************************************************************************************************************/
-int checkReallySorted(int* array,int size){
-  for(int i = 1;i<size-1;i++){
-      if(array[i] >= array[i+1]){
-          return 0;
-      }
-      
+    if (array[i] == array[i - 1] + array[i + 1])
+    {
+      return 1;
     }
-    return 1;
+  }
+  return 0;
 }
-#define SIZE 5
-int main() {
-    int arr[SIZE]= {1,2,7,8,10};
-    // Write C code here
-    printf("findIfSumNeighborns:%d", checkReallySorted(arr,SIZE));
-
-    return 0;
+int checkReallySorted(int *array, int size)
+{
+  for (int i = 1; i < size - 1; i++)
+  {
+    if (array[i] >= array[i + 1])
+    {
+      return 0;
+    }
+  }
+  return 1;
 }
-/********************************************************************************************************************************/
-void swapChar(char* ptr1, char* ptr2){
-char temp = *ptr1;
+void swapChar(char *ptr1, char *ptr2)
+{
+  char temp = *ptr1;
   *ptr1 = *ptr2;
-  *ptr2= temp;  
+  *ptr2 = temp;
 }
-void reverseArrayIterative(char* arr, int size){
-  for(int i = 0; i < size/2;i++){
+void reverseArrayIterative(char *arr, int size)
+{
+  for (int i = 0; i < size / 2; i++)
+  {
     swapChar(arr[i],arr[size-1-i];
   }
 }
-void reverseArrayRecursive(char* arr, int size){
-  if(size > 1){
-    swapChar(arr[i],arr[size-1-i];
+void reverseArrayRecursive(char *arr, int size)
+{
+  if (size > 1)
+  {
+    swapChar(arr[size],arr[size-1]);
     reverseArrayRecursive(arr + 1, size - 2);
   }
 }
-/********************************************************************************************************************************/
-
-
-
-
-
-
-
-
-
+int findSecondSmallValue(int *array, int size)
+{
+  int temp_data = 0;
+  if (size < 0)
+  {
+    return 0;
+  }
+  for (int i = 0; i < size; i++)
+  {
+    (array[i]) > (array[i + 1]) ? (temp_data = temp_data + array[i]) : (temp_data = temp_data + array[i + 1]);
+  }
+  return temp_data;
+}
